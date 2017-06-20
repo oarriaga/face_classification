7fe331597e3d82149a60b853bafd393a5031c73c
Alexis <alexisfcote@users.noreply.github.com>
2017-06-20 10:35:45 -0400

    Update utils.py
    Put fear instead of sad in accordance to the fer2013 dataset

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def get_labels(dataset_name):
     if dataset_name == 'fer2013':
-        return {0:'angry',1:'disgust',2:'sad',3:'happy',
+        return {0:'angry',1:'disgust',2:'fear',3:'happy',
                     4:'sad',5:'surprise',6:'neutral'}
     elif dataset_name == 'imdb':
         return {0:'woman', 1:'man'}
