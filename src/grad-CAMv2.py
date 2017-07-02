4d13ffdfaefbcba2724374edaf6a99c692c10ef2
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-07-02 12:12:23 +0200

    Cleaned/refactored grad-CAM visualization

--- src/grad-CAMv2.py
+++ src/grad-CAMv2.py
@@ -5,7 +5,6 @@ import keras.backend as K
 import tensorflow as tf
 import numpy as np
 import keras
-#import sys
 import cv2
 from utils.utils import preprocess_input
 from keras.models import load_model
@@ -13,12 +12,12 @@ import pickle
 import h5py
 
 def reset_optimizer_weights(model_filename):
-    f = h5py.File(model_filename, 'r+')
-    del f['optimizer_weights']
-    f.close()
+    model = h5py.File(model_filename, 'r+')
+    del model['optimizer_weights']
+    model.close()
 
-def target_category_loss(x, category_index, nb_classes):
-    return tf.multiply(x, K.one_hot([category_index], nb_classes))
+def target_category_loss(x, category_index, num_classes):
+    return tf.multiply(x, K.one_hot([category_index], num_classes))
 
 def target_category_loss_output_shape(input_shape):
     return input_shape
@@ -28,137 +27,121 @@ def normalize(x):
     return x / (K.sqrt(K.mean(K.square(x))) + 1e-5)
 
 def load_image():
-    img = pickle.load(open('test1.pkl','rb'))
-    #img_path = sys.argv[1]
-    #img = _imread(img_path)
-    #img = _imresize(img, (48, 48))
-    #img = image.load_img(img_path, target_size=(48, 48))
-    #x = image.img_to_array(img)
-    x = np.expand_dims(img, axis=0)
-    x = preprocess_input(x)
-    return x
+    image_array = pickle.load(open('test1.pkl','rb'))
+    image_array = np.expand_dims(image_array, axis=0)
+    image_array = preprocess_input(image_array)
+    return image_array
 
 def register_gradient():
     if "GuidedBackProp" not in ops._gradient_registry._registry:
         @ops.RegisterGradient("GuidedBackProp")
-        def _GuidedBackProp(op, grad):
+        def _GuidedBackProp(op, gradient):
             dtype = op.inputs[0].dtype
-            return grad * tf.cast(grad > 0., dtype) * \
-                tf.cast(op.inputs[0] > 0., dtype)
+            guided_gradient =  (gradient * tf.cast(gradient > 0., dtype) *
+                                tf.cast(op.inputs[0] > 0., dtype))
+            return guided_gradient
 
 def compile_saliency_function(model, activation_layer='conv2d_6'):
-    input_img = model.input
-    #layer_dict = dict([(layer.name, layer) for layer in model.layers[1:]])
-    layer_dict = dict([(layer.name, layer) for layer in model.layers])
-    layer_output = layer_dict[activation_layer].output
+    input_image = model.input
+    layer_output = model.get_layer(activation_layer).output
     max_output = K.max(layer_output, axis=3)
-    saliency = K.gradients(K.sum(max_output), input_img)[0]
-    return K.function([input_img, K.learning_phase()], [saliency])
+    saliency = K.gradients(K.sum(max_output), input_image)[0]
+    return K.function([input_image, K.learning_phase()], [saliency])
 
 def modify_backprop(model, name):
-    g = tf.get_default_graph()
-    with g.gradient_override_map({'Relu': name}):
+    graph = tf.get_default_graph()
+    with graph.gradient_override_map({'Relu': name}):
 
         # get layers that have an activation
-        #layer_dict = [layer for layer in model.layers[1:]
-        layer_dict = [layer for layer in model.layers
+        activation_layers = [layer for layer in model.layers
                       if hasattr(layer, 'activation')]
 
         # replace relu activation
-        for layer in layer_dict:
+        for layer in activation_layers:
             if layer.activation == keras.activations.relu:
                 layer.activation = tf.nn.relu
 
         # re-instanciate a new model
-        #new_model = VGG16(weights='imagenet')
         new_model = load_model('../trained_models/emotion_models/mini_XCEPTION.158-0.61.hdf5')
     return new_model
 
 def deprocess_image(x):
-    '''
-    Same normalization as in:
+    """ Same normalization as in:
     https://github.com/fchollet/keras/blob/master/examples/conv_filter_visualization.py
-    '''
+    """
     if np.ndim(x) > 3:
         x = np.squeeze(x)
     # normalize tensor: center on 0., ensure std is 0.1
-    x -= x.mean()
-    x /= (x.std() + 1e-5)
-    x *= 0.1
+    x = x - x.mean()
+    x = x / (x.std() + 1e-5)
+    x = x * 0.1
 
     # clip to [0, 1]
-    x += 0.5
+    x = x + 0.5
     x = np.clip(x, 0, 1)
 
     # convert to RGB array
-    x *= 255
+    x = x * 255
     if K.image_dim_ordering() == 'th':
         x = x.transpose((1, 2, 0))
     x = np.clip(x, 0, 255).astype('uint8')
     return x
 
-def grad_cam(input_model, image, category_index, layer_name):
+def calculate_gradient_weighted_CAM(input_model, image,
+                                    category_index, layer_name):
     model = Sequential()
     model.add(input_model)
 
-    #nb_classes = 1000
-    nb_classes = model.output_shape[1]
-    print(nb_classes)
-    target_layer = lambda x: target_category_loss(x, category_index, nb_classes)
+    num_classes = model.output_shape[1]
+    target_layer = lambda x: target_category_loss(x, category_index, num_classes)
     model.add(Lambda(target_layer,
                      output_shape = target_category_loss_output_shape))
 
     loss = K.sum(model.layers[-1].output)
     conv_output = model.layers[0].get_layer('conv2d_6').output
-    #conv_output =  [l for l in model.layers[0].layers if l.name is layer_name][0].output
-    grads = normalize(K.gradients(loss, conv_output)[0])
+    gradients = normalize(K.gradients(loss, conv_output)[0])
     gradient_function = K.function([model.layers[0].input, K.learning_phase()],
-                                                        [conv_output, grads])
+                                                    [conv_output, gradients])
 
-    output, grads_val = gradient_function([image, 0])
-    output, grads_val = output[0, :], grads_val[0, :, :, :]
+    output, evaluated_gradients = gradient_function([image, False])
+    output, evaluated_gradients = output[0, :], evaluated_gradients[0, :, :, :]
 
-    weights = np.mean(grads_val, axis = (0, 1))
-    cam = np.ones(output.shape[0 : 2], dtype = np.float32)
+    weights = np.mean(evaluated_gradients, axis = (0, 1))
+    CAM = np.ones(output.shape[0 : 2], dtype=np.float32)
 
-    for i, w in enumerate(weights):
-        cam += w * output[:, :, i]
+    for weight_arg, weight in enumerate(weights):
+        CAM = CAM + (weight * output[:, :, weight_arg])
 
-    cam = cv2.resize(cam, (48, 48))
-    cam = np.maximum(cam, 0)
-    heatmap = cam / np.max(cam)
+    CAM = cv2.resize(CAM, (48, 48))
+    CAM = np.maximum(CAM, 0)
+    heatmap = CAM / np.max(CAM)
 
     #Return to BGR [0..255] from the preprocessed image
     image = image[0, :]
-    image -= np.min(image)
+    image = image - np.min(image)
     image = np.minimum(image, 255)
 
-    cam = cv2.applyColorMap(np.uint8(255*heatmap), cv2.COLORMAP_JET)
-    cam = np.float32(cam) + np.float32(image)
-    cam = 255 * cam / np.max(cam)
-    return np.uint8(cam), heatmap
+    CAM = cv2.applyColorMap(np.uint8(255 * heatmap), cv2.COLORMAP_JET)
+    CAM = np.float32(CAM) + np.float32(image)
+    CAM = 255 * CAM / np.max(CAM)
+    return np.uint8(CAM), heatmap
 
+if __name__ == '__main__':
+    preprocessed_input = load_image()
+    model_filename = '../trained_models/emotion_models/mini_XCEPTION.158-0.61.hdf5'
+    #reset_optimizer_weights(model_filename)
+    model = load_model(model_filename)
 
-preprocessed_input = load_image()
-model_filename = '../trained_models/emotion_models/mini_XCEPTION.158-0.61.hdf5'
-#reset_optimizer_weights(model_filename)
+    predictions = model.predict(preprocessed_input)
+    predicted_class = np.argmax(predictions)
+    CAM, heatmap = calculate_gradient_weighted_CAM(model, preprocessed_input,
+                                                predicted_class, 'conv2d_6')
+    cv2.imwrite('gradcam.jpg', CAM)
 
-model = load_model('../trained_models/emotion_models/mini_XCEPTION.158-0.61.hdf5')
-#odel = VGG16(weights='imagenet')
+    register_gradient()
+    guided_model = modify_backprop(model, 'GuidedBackProp')
+    get_saliency = compile_saliency_function(guided_model)
+    saliency = get_saliency([preprocessed_input, 0])
+    gradCAM = saliency[0] * heatmap[..., np.newaxis]
+    cv2.imwrite('guided_gradcam.jpg', deprocess_image(gradCAM))
 
-predictions = model.predict(preprocessed_input)
-#top_1 = decode_predictions(predictions)[0][0]
-#print('Predicted class:')
-#print('%s (%s) with probability %.2f' % (top_1[1], top_1[0], top_1[2]))
-
-predicted_class = np.argmax(predictions)
-print(predicted_class)
-cam, heatmap = grad_cam(model, preprocessed_input, predicted_class, "conv2d_6")
-cv2.imwrite("gradcam.jpg", cam)
-
-register_gradient()
-guided_model = modify_backprop(model, 'GuidedBackProp')
-saliency_fn = compile_saliency_function(guided_model)
-saliency = saliency_fn([preprocessed_input, 0])
-gradcam = saliency[0] * heatmap[..., np.newaxis]
-cv2.imwrite("guided_gradcam.jpg", deprocess_image(gradcam))
