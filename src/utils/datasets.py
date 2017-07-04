3efc81a06fd01b08977d91c4e5380e806c2ae563
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-07-04 11:17:48 +0200

    Added new KDEF dataset loading capability and resize option for fer2013 and KDEF

--- src/utils/datasets.py
+++ src/utils/datasets.py
@@ -2,20 +2,25 @@ from scipy.io import loadmat
 import pandas as pd
 import numpy as np
 from random import shuffle
+import os
+import cv2
 
 class DataManager(object):
     """Class for loading fer2013 emotion classification dataset or
         imdb gender classification dataset."""
-    def __init__(self, dataset_name='imdb', dataset_path=None):
+    def __init__(self, dataset_name='imdb', dataset_path=None, image_size=(48, 48)):
 
         self.dataset_name = dataset_name
         self.dataset_path = dataset_path
+        self.image_size = image_size
         if self.dataset_path != None:
             self.dataset_path = dataset_path
         elif self.dataset_name == 'imdb':
             self.dataset_path = '../datasets/imdb_crop/imdb.mat'
         elif self.dataset_name == 'fer2013':
             self.dataset_path = '../datasets/fer2013/fer2013.csv'
+        elif self.dataset_name == 'KDEF':
+            self.dataset_path = '../datasets/KDEF/'
         else:
             raise Exception('Incorrect dataset name, please input imdb or fer2013')
 
@@ -24,6 +29,8 @@ class DataManager(object):
             ground_truth_data = self._load_imdb()
         elif self.dataset_name == 'fer2013':
             ground_truth_data = self._load_fer2013()
+        elif self.dataset_name == 'KDEF':
+            ground_truth_data = self._load_KDEF()
         return ground_truth_data
 
     def _load_imdb(self):
@@ -54,11 +61,41 @@ class DataManager(object):
         for pixel_sequence in pixels:
             face = [int(pixel) for pixel in pixel_sequence.split(' ')]
             face = np.asarray(face).reshape(width, height)
-            faces.append(face)
+            face = cv2.resize(face.astype('uint8'), self.image_size)
+            faces.append(face.astype('float32'))
         faces = np.asarray(faces)
         faces = np.expand_dims(faces, -1)
         emotions = pd.get_dummies(data['emotion']).as_matrix()
         return faces, emotions
+
+    def _load_KDEF(self):
+        class_to_arg = get_class_to_arg(self.dataset_name)
+        num_classes = len(class_to_arg)
+
+        file_paths = []
+        for folder, subfolders, filenames in os.walk(self.dataset_path):
+            for filename in filenames:
+                if filename.lower().endswith(('.jpg')):
+                    file_paths.append(os.path.join(folder, filename))
+
+        num_faces = len(file_paths)
+        y_size, x_size = self.image_size
+        faces = np.zeros(shape=(num_faces, y_size, x_size))
+        emotions = np.zeros(shape=(num_faces, num_classes))
+        for file_arg, file_path in enumerate(file_paths):
+            image_array = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+            image_array = cv2.resize(image_array, (y_size, x_size))
+            faces[file_arg] = image_array
+            file_basename = os.path.basename(file_path)
+            file_emotion = file_basename[4:6]
+            # there are two file names in the dataset that don't match the given classes
+            try:
+                emotion_arg = class_to_arg[file_emotion]
+            except:
+                continue
+            emotions[file_arg, emotion_arg] = 1
+        return faces, emotions
+
 
 def get_labels(dataset_name):
     if dataset_name == 'fer2013':
@@ -66,6 +103,19 @@ def get_labels(dataset_name):
                 4:'sad',5:'surprise',6:'neutral'}
     elif dataset_name == 'imdb':
         return {0:'woman', 1:'man'}
+    elif dataset_name == 'KDEF':
+        return {0:'AN', 1:'DI', 2:'AF', 3:'HA', 4:'SA', 5:'SU', 6:'NE'}
+    else:
+        raise Exception('Invalid dataset name')
+
+def get_class_to_arg(dataset_name='fer2013'):
+    if dataset_name == 'fer2013':
+        return {'angry':0, 'disgust':1, 'fear':2, 'happy':3, 'sad':4,
+                'surprise':5, 'neutral':6}
+    elif dataset_name == 'imdb':
+        return {'woman':0, 'man':1}
+    elif dataset_name == 'KDEF':
+        return {'AN':0, 'DI':1, 'AF':2, 'HA':3, 'SA':4, 'SU':5, 'NE':6}
     else:
         raise Exception('Invalid dataset name')
 
