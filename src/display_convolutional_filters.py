891d2e8c0fad96e284cee842dd29706075c2e803
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-07-05 17:01:30 +0200

    Fixed bugs with new image size

--- src/display_convolutional_filters.py
+++ src/display_convolutional_filters.py
@@ -4,12 +4,18 @@ from keras.models import load_model
 import keras.backend as K
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
 
 from utils.visualizer import pretty_imshow
 from utils.visualizer import make_mosaic
 from utils.preprocessor import preprocess_input
 
-model = load_model('../trained_models/gender_models/simple_CNN.81-0.96.hdf5')
+model = load_model('../trained_models/emotion_models/fer2013_mini_XCEPTION.102-0.66.hdf5',
+                                                compile=False)
+
+model.compile(optimizer='adam', loss='categorical_crossentropy',
+                                            metrics=['accuracy'])
+
 conv_weights = model.layers[1].get_weights()
 kernel_conv_weights = conv_weights[0]
 kernel_conv_weights = np.squeeze(kernel_conv_weights)
@@ -32,9 +38,11 @@ get_feature_map = K.function([model.layers[0].input, K.learning_phase()],
 
 faces = pickle.load(open('utils/faces.pkl', 'rb'))
 face = faces[0]
+face = cv2.resize(face.astype('uint8'), (64, 64))
 face = preprocess_input(face)
 face = np.expand_dims(face, 0)
-feature_map = get_feature_map((face,False))[0]
+face = np.expand_dims(face, -1)
+feature_map = get_feature_map((face, False))[0]
 feature_map = np.rollaxis(feature_map, 3, 0)
 feature_map = np.rollaxis(feature_map, 1, 4)
 print(feature_map.shape)
