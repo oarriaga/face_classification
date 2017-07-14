0fb15ab594c913c610d039dc7a32b6718250cc4e
Octavio Arriaga <arriaga.camargo@gmail.com>
2017-07-14 11:54:29 +0200

    Added pep8 conventions to indentation levels

--- src/train_gender_classifier.py
+++ src/train_gender_classifier.py
@@ -6,7 +6,8 @@ Github: https://github.com/oarriaga
 Description: Train gender classification model
 """
 
-from keras.callbacks import CSVLogger, ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+from keras.callbacks import CSVLogger, ModelCheckpoint, EarlyStopping
+from keras.callbacks import ReduceLROnPlateau
 from utils.datasets import DataManager
 from models.cnn import mini_XCEPTION
 from utils.data_augmentation import ImageGenerator
@@ -34,24 +35,24 @@ train_keys, val_keys = split_imdb_data(ground_truth_data, validation_split)
 print('Number of training samples:', len(train_keys))
 print('Number of validation samples:', len(val_keys))
 image_generator = ImageGenerator(ground_truth_data, batch_size,
-                                input_shape[:2],
-                                train_keys, val_keys, None,
-                                path_prefix=images_path,
-                                vertical_flip_probability=0,
-                                grayscale=grayscale,
-                                do_random_crop=do_random_crop)
+                                 input_shape[:2],
+                                 train_keys, val_keys, None,
+                                 path_prefix=images_path,
+                                 vertical_flip_probability=0,
+                                 grayscale=grayscale,
+                                 do_random_crop=do_random_crop)
 
 # model parameters/compilation
 model = mini_XCEPTION(input_shape, num_classes)
 model.compile(optimizer='adam',
-            loss='categorical_crossentropy',
-            metrics=['accuracy'])
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
 model.summary()
 
 # model callbacks
 early_stop = EarlyStopping('val_loss', patience=patience)
 reduce_lr = ReduceLROnPlateau('val_loss', factor=0.1,
-                patience=int(patience/2), verbose=1)
+                              patience=int(patience/2), verbose=1)
 csv_logger = CSVLogger(log_file_path, append=False)
 model_names = trained_models_path + '.{epoch:02d}-{val_acc:.2f}.hdf5'
 model_checkpoint = ModelCheckpoint(model_names,
@@ -66,6 +67,5 @@ model.fit_generator(image_generator.flow(mode='train'),
                     steps_per_epoch=int(len(train_keys) / batch_size),
                     epochs=num_epochs, verbose=1,
                     callbacks=callbacks,
-                    validation_data= image_generator.flow('val'),
+                    validation_data=image_generator.flow('val'),
                     validation_steps=int(len(val_keys) / batch_size))
-
