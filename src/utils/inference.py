d198d8b8a22d966e941a4913c8604c08f58daa6a
Gabriel Bianconi <bianconi.gabriel@gmail.com>
2018-10-03 02:01:25 -0400

    Update load_img for Keras' new interface

--- src/utils/inference.py
+++ src/utils/inference.py
@@ -4,7 +4,11 @@ import numpy as np
 from keras.preprocessing import image
 
 def load_image(image_path, grayscale=False, target_size=None):
-    pil_image = image.load_img(image_path, grayscale, target_size)
+    if grayscale:
+        pil_image = image.load_img(image_path, color_mode='grayscale',
+                                   target_size=target_size)
+    else:
+        pil_image = image.load_img(image_path, target_size=target_size)
     return image.img_to_array(pil_image)
 
 def load_detection_model(model_path):
