86ecdb507dd18065f945b643a2eb5077ef07f01d
Octavio Arriaga <arriaga.camargo@gmail.com>
2019-04-15 18:38:26 +0200

    Refactor of DenseNet

--- src/models/densenet.py
+++ src/models/densenet.py
@@ -3,107 +3,50 @@ import os
 from keras import layers
 from keras import backend
 from keras import models
+from keras.layers import Input
 
 from .blocks import dense_block
 from .blocks import transition_block
 
 
-def build_densenet(
-        blocks,
-        include_top=True,
-        weights='imagenet',
-        input_tensor=None,
-        input_shape=None,
-        pooling=None,
-        classes=1000,
-        **kwargs):
+def build_densenet(input_shape, num_classes, kernels_per_block):
     """Instantiates the DenseNet architecture.
-    Optionally loads weights pre-trained on ImageNet.
-    Note that the data format convention used by the model is
-    the one specified in your Keras config at `~/.keras/keras.json`.
     # Arguments
-        blocks: numbers of building blocks for the four dense layers.
-        include_top: whether to include the fully-connected
-            layer at the top of the network.
-        weights: one of `None` (random initialization),
-              'imagenet' (pre-training on ImageNet),
-              or the path to the weights file to be loaded.
-        input_tensor: optional Keras tensor
-            (i.e. output of `layers.Input()`)
-            to use as image input for the model.
         input_shape: optional shape tuple, only to be specified
             if `include_top` is False. It should have 3 inputs channels.
-        pooling: optional pooling mode for feature extraction
-            when `include_top` is `False`.
-            - `None` means that the output of the model will be
-                the 4D tensor output of the
-                last convolutional block.
-            - `avg` means that global average pooling
-                will be applied to the output of the
-                last convolutional block, and thus
-                the output of the model will be a 2D tensor.
-            - `max` means that global max pooling will
-                be applied.
-        classes: optional number of classes to classify images
+        num_classes: optional number of num_classes to classify images
             into, only to be specified if `include_top` is True, and
             if no `weights` argument is specified.
+        kernels_per_block: numbers of kernels per dense blocks.
     # Returns
         A Keras model instance.
-    # Raises
-        ValueError: in case of invalid argument for `weights`,
-            or invalid input shape.
     """
 
-    if not (weights in {'imagenet', None} or os.path.exists(weights)):
-        raise ValueError('The `weights` argument should be either '
-                         '`None` (random initialization), `imagenet` '
-                         '(pre-training on ImageNet), '
-                         'or the path to the weights file to be loaded.')
-
-    if weights == 'imagenet' and include_top and classes != 1000:
-        raise ValueError(
-            'If using `weights` as `"imagenet" ` with `include_top`'
-            ' as true, `classes` should be 1000')
-
-    if input_tensor is None:
-        img_input = layers.Input(shape=input_shape)
-    else:
-        if not backend.is_keras_tensor(input_tensor):
-            img_input = layers.Input(tensor=input_tensor, shape=input_shape)
-        else:
-            img_input = input_tensor
-
     bn_axis = 3 if backend.image_data_format() == 'channels_last' else 1
+    inputs = Input(input_shape, name='inputs')
 
-    x = layers.ZeroPadding2D(padding=((3, 3), (3, 3)))(img_input)
-    x = layers.Conv2D(64, 7, strides=2, use_bias=False, name='conv1/conv')(x)
+    x = layers.Conv2D(32, 3, use_bias=False, padding='same',
+                      name='conv1a/conv')(inputs)
     x = layers.BatchNormalization(
-        axis=bn_axis, epsilon=1.001e-5, name='conv1/bn')(x)
-    x = layers.Activation('relu', name='conv1/relu')(x)
-    x = layers.ZeroPadding2D(padding=((1, 1), (1, 1)))(x)
-    x = layers.MaxPooling2D(3, strides=2, name='pool1')(x)
+        axis=bn_axis, epsilon=1.001e-5, name='conv1a/bn')(x)
+    x = layers.Activation('relu', name='conv1a/relu')(x)
 
-    for blocks_arg, num_blocks in enumerate(blocks[:-1], 1):
+    x = layers.Conv2D(64, 3, use_bias=False, padding='same',
+                      name='conv1b/conv')(x)
+    x = layers.BatchNormalization(
+        axis=bn_axis, epsilon=1.001e-5, name='conv1b/bn')(x)
+    x = layers.Activation('relu', name='conv1b/relu')(x)
+
+    for blocks_arg, num_blocks in enumerate(kernels_per_block[:-1], 1):
         x = dense_block(x, num_blocks, name='conv%i' % blocks_arg)
         x = transition_block(x, 0.5, name='pool%i' % blocks_arg)
 
-    x = dense_block(x, blocks[-1], name='conv%i' % len(blocks))
+    x = dense_block(x, kernels_per_block[-1],
+                    name='conv%i' % len(kernels_per_block))
     x = layers.BatchNormalization(
         axis=bn_axis, epsilon=1.001e-5, name='bn')(x)
     x = layers.Activation('relu', name='relu')(x)
 
-    if include_top:
-        x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
-        x = layers.Dense(classes, activation='softmax', name='fc1000')(x)
-    else:
-        if pooling == 'avg':
-            x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
-        elif pooling == 'max':
-            x = layers.GlobalMaxPooling2D(name='max_pool')(x)
-
-    # Ensure that the model takes into account
-    # any potential predecessors of `input_tensor`.
-    inputs = img_input
-
-    # Create model.
+    x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
+    x = layers.Dense(num_classes, activation='softmax', name='fc1000')(x)
     return models.Model(inputs, x, name='DenseNet')
