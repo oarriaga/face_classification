184a4a8446cd3a6e862b9ccc28ce7f3f3652d55b
Octavio Arriaga <arriaga.camargo@gmail.com>
2019-04-16 11:53:37 +0200

    Add stem block kernel and growth rate as hyper-parameters

--- src/models/densenet.py
+++ src/models/densenet.py
@@ -1,5 +1,3 @@
-import os
-
 from keras import layers
 from keras import backend
 from keras import models
@@ -9,7 +7,8 @@ from .blocks import dense_block
 from .blocks import transition_block
 
 
-def build_densenet(input_shape, num_classes, kernels_per_block):
+def build_densenet(input_shape, num_classes, stem_kernels,
+                   blocks, growth_rate=32):
     """Instantiates the DenseNet architecture.
     # Arguments
         input_shape: optional shape tuple, only to be specified
@@ -17,32 +16,27 @@ def build_densenet(input_shape, num_classes, kernels_per_block):
         num_classes: optional number of num_classes to classify images
             into, only to be specified if `include_top` is True, and
             if no `weights` argument is specified.
-        kernels_per_block: numbers of kernels per dense blocks.
+        block_kernels: numbers of kernels per dense blocks.
     # Returns
         A Keras model instance.
     """
 
     bn_axis = 3 if backend.image_data_format() == 'channels_last' else 1
-    inputs = Input(input_shape, name='inputs')
 
-    x = layers.Conv2D(32, 3, use_bias=False, padding='same',
-                      name='conv1a/conv')(inputs)
-    x = layers.BatchNormalization(
-        axis=bn_axis, epsilon=1.001e-5, name='conv1a/bn')(x)
-    x = layers.Activation('relu', name='conv1a/relu')(x)
+    x = inputs = Input(input_shape, name='inputs')
+    for stem_arg, num_kernels in enumerate(stem_kernels):
+        x = layers.Conv2D(stem_kernels[0], 3, use_bias=False, padding='same',
+                          name='conv/conv%i' % stem_arg)(inputs)
+        x = layers.BatchNormalization(
+            axis=bn_axis, epsilon=1.001e-5, name='conv/bn%i' % stem_arg)(x)
+        x = layers.Activation('relu', name='conv/relu%i' % stem_arg)(x)
 
-    x = layers.Conv2D(64, 3, use_bias=False, padding='same',
-                      name='conv1b/conv')(x)
-    x = layers.BatchNormalization(
-        axis=bn_axis, epsilon=1.001e-5, name='conv1b/bn')(x)
-    x = layers.Activation('relu', name='conv1b/relu')(x)
+    for block_arg, num_blocks in enumerate(blocks[:-1]):
+        x = dense_block(x, num_blocks, growth_rate, name='conv%i' % block_arg)
+        x = transition_block(x, 0.5, name='pool%i' % block_arg)
 
-    for blocks_arg, num_blocks in enumerate(kernels_per_block[:-1], 1):
-        x = dense_block(x, num_blocks, name='conv%i' % blocks_arg)
-        x = transition_block(x, 0.5, name='pool%i' % blocks_arg)
-
-    x = dense_block(x, kernels_per_block[-1],
-                    name='conv%i' % len(kernels_per_block))
+    name = 'conv%i' % len(blocks)
+    x = dense_block(x, blocks[-1], growth_rate, name=name)
     x = layers.BatchNormalization(
         axis=bn_axis, epsilon=1.001e-5, name='bn')(x)
     x = layers.Activation('relu', name='relu')(x)
