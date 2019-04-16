a937c24f31618861fbbba0d932b45907c6d37e3d
Octavio Arriaga <arriaga.camargo@gmail.com>
2019-04-16 16:29:06 +0200

    Fix bugs of data manager and argparser labels

--- src/train.py
+++ src/train.py
@@ -18,39 +18,39 @@ from models import build_xception, build_densenet, build_vgg
 
 description = 'Training real-time CNNs for emotion and sex classification'
 parser = argparse.ArgumentParser(description=description)
-parser.add_argument('--dataset', default='FERPlus',
+parser.add_argument('-d', '--dataset', default='FERPlus',
                     choices=['FERPlus', 'IMDB'], type=str,
                     help='FERPlus or IMDB')
-parser.add_argument('--batch_size', default=32, type=int,
+parser.add_argument('-bs', '--batch_size', default=32, type=int,
                     help='Batch size for training')
-parser.add_argument('--learning_rate', default=0.002, type=float,
+parser.add_argument('-lr', '--learning_rate', default=0.002, type=float,
                     help='Initial learning rate for Nadam')
-parser.add_argument('--image_size', default=128, type=int,
+parser.add_argument('-is', '--image_size', default=128, type=int,
                     help='Size of the side of a square image e.g. 64')
-parser.add_argument('--stop_patience', default=9, type=int,
+parser.add_argument('-sp', '--stop_patience', default=12, type=int,
                     help='Number of epochs before doing early stopping')
-parser.add_argument('--plateau_patience', default=4, type=int,
+parser.add_argument('-pp', '--plateau_patience', default=5, type=int,
                     help='Number of epochs before reducing learning rate')
-parser.add_argument('--max_num_epochs', default=10000, type=int,
+parser.add_argument('-e', '--max_num_epochs', default=10000, type=int,
                     help='Maximum number of epochs before finishing')
-parser.add_argument('--loss', default='categorical_crossentropy', type=str,
-                    help='Classification loss')
-parser.add_argument('--stem_kernels', nargs='+', default=[32, 64], type=int,
-                    help='Kernels used in the stem block e.g. 32, 64')
-parser.add_argument('--block_data', nargs='+',
+parser.add_argument('-l', '--loss', default='categorical_crossentropy',
+                    type=str, help='Classification loss')
+parser.add_argument('-sk', '--stem_kernels', nargs='+', default=[32, 64],
+                    type=int, help='Kernels used in the stem block')
+parser.add_argument('-bd', '--block_data', nargs='+',
                     default=[128, 128, 256, 256, 512, 512, 1024], type=int,
                     help='If model is `vgg` or `xception` list should \
                     contain the  number of kernels per block. If `densenet` \
                     list contains the number of dense blocks per \
                     transition block')
-parser.add_argument('--class_weight', nargs='+', type=int,
+parser.add_argument('-cw', '--class_weight', nargs='+', type=int,
                     default=[1.0, 1.3, 2.8, 2.9, 4.1, 53.5, 15.8, 62.2],
                     help='Kernels used in each block e.g. 128 256 512')
-parser.add_argument('--model_name', default='xception',
+parser.add_argument('-m', '--model_name', default='xception',
                     choices=['xception', 'densenet', 'vgg'], type=str,
                     help='CNN model structure')
-parser.add_argument('--save_path', default='../trained_models/', type=str,
-                    help='Path for writing model weights and logs')
+parser.add_argument('-s', '--save_path', default='../trained_models/',
+                    type=str, help='Path for writing model weights and logs')
 args = parser.parse_args()
 
 if args.dataset == 'FERPlus':
@@ -64,7 +64,7 @@ input_shape = (args.image_size, args.image_size, 1)
 for split in ['train', 'val', 'test']:
     data_manager = DataManager(split, input_shape[:2])
     data_managers.append(data_manager)
-    faces, labels = data_managers.load_data()
+    faces, labels = data_manager.load_data()
     datasets.append([faces / 255.0, labels])
 
 # data generator and augmentations
@@ -79,7 +79,7 @@ data_augmentator = ImageDataGenerator(
 model_builder = {
     'xception': build_xception, 'densenet': build_densenet, 'vgg': build_vgg}
 optimizer = Nadam(args.learning_rate)
-num_classes = data_manager[0].num_classes
+num_classes = data_managers[0].num_classes
 model_builder = model_builder[args.model_name]
 model_inputs = (input_shape, num_classes, args.stem_kernels, args.block_data)
 model = model_builder(*model_inputs)
@@ -92,7 +92,7 @@ save_path = os.path.join(args.save_path, args.dataset, model.name + date)
 if not os.path.exists(save_path):
     os.makedirs(save_path)
 logger = CSVLogger(os.path.join(save_path, model.name + '_optimization.log'))
-weights_path = os.path.join(save_path, '_'.join(model.name, 'weights.hdf5'))
+weights_path = os.path.join(save_path, '_'.join([model.name, 'weights.hdf5']))
 checkpoint = ModelCheckpoint(weights_path, verbose=1, save_weights_only=True)
 early_stop = EarlyStopping(patience=args.stop_patience)
 reduce_lr = ReduceLROnPlateau(patience=args.plateau_patience, verbose=1)
