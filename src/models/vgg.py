4646e4c2ce7d37560870b9dd83bb3147edfbe2f2
Octavio Arriaga <arriaga.camargo@gmail.com>
2019-04-16 11:54:18 +0200

    Add stem block kernel as hyper-parameter

--- src/models/vgg.py
+++ src/models/vgg.py
@@ -1,30 +1,29 @@
 from keras.layers import Activation, Conv2D
 from keras.layers import BatchNormalization
+from keras.layers import MaxPool2D
 from keras.layers import GlobalAveragePooling2D
 from keras.layers import Input
 from keras.models import Model
 
 
-def build_vgg(input_shape, num_classes, kernels_per_block):
+def build_vgg(input_shape, num_classes, stem_kernels, block_kernels):
 
-    inputs = Input(input_shape, name='inputs')
-    x = Conv2D(32, 3, padding='same')(inputs)
-    x = BatchNormalization()
-    x = Activation('relu')(x)
-
-    x = Conv2D(64, 3, padding='same')(x)
-    x = BatchNormalization()
-    x = Activation('relu')(x)
-
-    for num_kernels in kernels_per_block:
-        x = Conv2D(x, num_kernels)
-        x = BatchNormalization()
+    x = inputs = Input(input_shape, name='inputs')
+    for num_kernels in stem_kernels:
+        x = Conv2D(num_kernels, 3, padding='same')(x)
+        x = BatchNormalization()(x)
         x = Activation('relu')(x)
 
+    for num_kernels in block_kernels:
+        x = Conv2D(num_kernels, 3, padding='same')(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+        x = MaxPool2D(2)(x)
+
     x = Conv2D(num_classes, 3, padding='same')(x)
-    x = BatchNormalization()
+    x = BatchNormalization()(x)
     x = GlobalAveragePooling2D()(x)
     output = Activation('softmax', name='predictions')(x)
-    model_name = ''.join(['VGG', str(len(kernels_per_block))])
+    model_name = ''.join(['VGG', str(len(block_kernels))])
     model = Model(inputs, output, name=model_name)
     return model
