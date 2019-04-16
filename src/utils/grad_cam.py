a6be67778ff18c919d9a8f852637d3742406825d
Octavio Arriaga <arriaga.camargo@gmail.com>
2019-04-16 13:25:14 +0200

    Add old preprocessing to script

--- src/utils/grad_cam.py
+++ src/utils/grad_cam.py
@@ -9,7 +9,14 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.python.framework import ops
 
-from .preprocessor import preprocess_input
+
+def preprocess_input(x, v2=True):
+    x = x.astype('float32')
+    x = x / 255.0
+    if v2:
+        x = x - 0.5
+        x = x * 2.0
+    return x
 
 
 def reset_optimizer_weights(model_filename):
