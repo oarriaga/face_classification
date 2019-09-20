e80a4dfc5a84429a157605dbcc6354eefccdd855
Octavio Arriaga <octavio.arriaga@dfki.de>
2019-09-20 15:27:04 +0200

    Start general demo script with cleaner API

---
+++ src/demo.py
@@ -0,0 +1,20 @@
+from processing import DetectionPipeline
+from processing import VideoPlayer
+from models import HaarCascadeDetector
+from keras.models import load_model
+from datasets import get_labels
+
+
+# parameters for loading data and images
+detector_path = '../trained_models/detection_models/haarcascade_frontalface_default.xml'
+classifier_path = '../trained_models/emotion_models/fer2013_mini_XCEPTION.102-0.66.hdf5'
+
+# hyper-parameters for bounding boxes shape
+image_size, offsets, class_names = (640, 480), (20, 40), get_labels('FERPlus')
+
+# loading models
+classifier = load_model(classifier_path, compile=False)
+detector = HaarCascadeDetector(detector_path)
+pipeline = DetectionPipeline(detector, classifier, offsets, class_names)
+video_player = VideoPlayer(image_size, pipeline, camera=0)
+video_player.start()
