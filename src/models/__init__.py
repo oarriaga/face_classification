e97662cef37f4fc6d523962195fcec412d55b3d4
Octavio Arriaga <octavio.arriaga@dfki.de>
2019-09-20 15:25:34 +0200

    Add detection model to module imports

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,3 +2,4 @@ from .xception import build_xception
 from .densenet import build_densenet
 from .vgg import build_vgg
 from .blocks import non_local_block
+from .detection import HaarCascadeDetector
