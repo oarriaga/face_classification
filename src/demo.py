e5f0f1a1950667860efe0fca60ba5873c19ae759
Octavio Arriaga <octavio.arriaga@dfki.de>
2019-09-23 15:16:39 +0200

    Change original FER dataset name convention

--- src/demo.py
+++ src/demo.py
@@ -10,7 +10,7 @@ detector_path = '../trained_models/detection_models/haarcascade_frontalface_defa
 classifier_path = '../trained_models/emotion_models/fer2013_mini_XCEPTION.102-0.66.hdf5'
 
 # hyper-parameters for bounding boxes shape
-image_size, offsets, class_names = (640, 480), (20, 40), get_labels('FER2013')
+image_size, offsets, class_names = (640, 480), (20, 40), get_labels('FER')
 
 # loading models
 classifier = load_model(classifier_path, compile=False)
@@ -18,3 +18,6 @@ detector = HaarCascadeDetector(detector_path)
 pipeline = DetectionPipeline(detector, classifier, offsets, class_names)
 video_player = VideoPlayer(image_size, pipeline, camera=0)
 video_player.start()
+
+# check if adding batch normalization is done in original Xception
+# model = build_xception((64, 64, 1), 7, [8, 8], [16, 32, 64, 128])
