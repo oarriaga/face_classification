e7b6a2f0b8365582b5151162b932cce808c7be3e
Octavio Arriaga <octavio.arriaga@dfki.de>
2019-09-23 15:12:16 +0200

    Remove dependency on pandas for loading csv files

--- src/datasets/ferplus.py
+++ src/datasets/ferplus.py
@@ -1,17 +1,18 @@
-import pandas as pd
 import numpy as np
 import cv2
 
+from .utils import get_labels
+
 IMAGES_PATH = '../datasets/fer2013/fer2013.csv'
-LABELS_PATH = '../datasets/fer2013new.csv'
+LABELS_PATH = '../datasets/fer2013/fer2013new.csv'
 
 
 class FERPlus(object):
-    """Class for loading FER2013 [1] emotion classification dataset with
-    the FERPlus labels [2]:
+    """Class for loading FERPlus. FERPlus contains faces from FER [1] with
+    the new labels made in [2]:
     [1] kaggle.com/c/challenges-in-representation-learning-facial-\
             expression-recognition-challenge
-    [2] github.com/Microsoft/FERPlu://github.com/Microsoft/FERPlus"""
+    [2] https://github.com/microsoft/FERPlus/blob/master/fer2013new.csv"""
 
     def __init__(self, split='train', image_size=(48, 48),
                  dataset_name='FERPlus'):
@@ -21,30 +22,22 @@ class FERPlus(object):
         self.dataset_name = dataset_name
         self.images_path = IMAGES_PATH
         self.labels_path = LABELS_PATH
-        self.class_names = ['neutral', 'happiness', 'surprise', 'sadness',
-                            'anger', 'disgust', 'fear', 'contempt']
+        self.class_names = get_labels('FERPlus')
         self.num_classes = len(self.class_names)
-        self.arg_to_name = dict(zip(range(self.num_classes), self.class_names))
-        self.name_to_arg = dict(zip(self.class_names, range(self.num_classes)))
-        self._split_to_filter = {
+        self.split_to_filter = {
             'train': 'Training', 'val': 'PublicTest', 'test': 'PrivateTest'}
 
     def load_data(self):
-        filter_name = self._split_to_filter[self.split]
-        pixel_sequences = pd.read_csv(self.images_path)
-        pixel_sequences = pixel_sequences[pixel_sequences.Usage == filter_name]
-        pixel_sequences = pixel_sequences['pixels'].tolist()
-        faces = []
-        for pixel_sequence in pixel_sequences:
-            face = [float(pixel) for pixel in pixel_sequence.split(' ')]
-            face = np.asarray(face).reshape(48, 48)
-            faces.append(cv2.resize(face, self.image_size))
-        faces = np.asarray(faces)
-        faces = np.expand_dims(faces, -1)
+        data = np.genfromtxt(self.images_path, str, '#', ',', 1)
+        data = data[data[:, -1] == self.split_to_filter[self.split]]
+        faces = np.zeros((len(data), *self.image_size, 1))
+        for sample_arg, sample in enumerate(data):
+            face = np.array(sample[1].split(' '), dtype=int).reshape(48, 48)
+            faces[sample_arg, :, :, 0] = cv2.resize(face, self.image_size)
 
-        emotions = pd.read_csv(self.labels_path)
-        emotions = emotions[emotions.Usage == filter_name]
-        emotions = emotions.iloc[:, 2:10].values
+        emotions = np.genfromtxt(self.labels_path, str, '#', ',', 1)
+        emotions = emotions[emotions[:, 0] == self.split_to_filter[self.split]]
+        emotions = emotions[:, 2:10].astype(float)
         N = np.sum(emotions, axis=1)
         mask = N != 0
         N, faces, emotions = N[mask], faces[mask], emotions[mask]
