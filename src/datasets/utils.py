8f7281efdcb94cbe4e3dea9dc65a06c44ec5e182
Octavio Arriaga <octavio.arriaga@dfki.de>
2019-09-23 15:13:16 +0200

    Change FER2013 to FER dataset for better distinction between FERPlus

--- src/datasets/utils.py
+++ src/datasets/utils.py
@@ -3,7 +3,7 @@ def get_labels(dataset='FERPlus'):
     if dataset == 'FERPlus':
         return ['neutral', 'happiness', 'surprise', 'sadness',
                 'anger', 'disgust', 'fear', 'contempt']
-    elif dataset == 'FER2013':
+    elif dataset == 'FER':
         return ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise',
                 'neutral']
     elif dataset == 'IMDB':
