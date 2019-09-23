a045f6a050d5bbaa8b3393b3761395abd1cba5aa
Octavio Arriaga <octavio.arriaga@dfki.de>
2019-09-23 10:59:12 +0200

    Add label names in order for FER2013 dataset

--- src/datasets/utils.py
+++ src/datasets/utils.py
@@ -3,6 +3,9 @@ def get_labels(dataset='FERPlus'):
     if dataset == 'FERPlus':
         return ['neutral', 'happiness', 'surprise', 'sadness',
                 'anger', 'disgust', 'fear', 'contempt']
+    elif dataset == 'FER2013':
+        return ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise',
+                'neutral']
     elif dataset == 'IMDB':
         return ['man', 'woman']
     else:
